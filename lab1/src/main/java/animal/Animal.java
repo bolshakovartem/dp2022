349518d3d861915cb3fb92d7452dc69856b0c0a9
bolshakovartem <89931102+bolshakovartem@users.noreply.github.com>
2022-02-05 16:29:45 +0200

    Add files via upload

---
+++ lab1/src/main/java/animal/Animal.java
@@ -0,0 +1,36 @@
+package animal;
+
+public class Animal {
+	private String title;
+	private int age;
+	private String info;
+	public Animal(String title, int age, String info) {
+		super();
+		this.title = title;
+		this.age = age;
+		this.info = info;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getInfo() {
+		return info;
+	}
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	@Override
+	public String toString() {
+		return "{\"title\":\""+title+"\",\"age\":"+age+", \"info\":\""+info+"\"}";
+	}
+	
+}
---
+++ lab1/src/main/java/servlet/Servlet1.java
@@ -0,0 +1,32 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import animal.Animal;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+@WebServlet("/Servlet1")
+public class Servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("application/json");
+		PrintWriter out = response.getWriter();
+		
+		Animal animal = new Animal("Tiger", 10, "The biggest cat in the world with black stripes");
+		out.println("["+animal+"]");
+	}
+
+}
