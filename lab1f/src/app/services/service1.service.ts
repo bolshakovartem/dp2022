eb17fa44a1b71dc56f0145ee39041cfbd4f2f3e2
bolshakovartem <artemis4650@gmail.com>
2022-02-14 17:44:48 +0200

    qwerty

--- lab1f/src/app/services/service1.service.ts
+++ lab1f/src/app/services/service1.service.ts
@@ -7,12 +7,22 @@ import { Animal } from '../interfaces/animal';
   providedIn: 'root'
 })
 export class Service1Service {
-url:string = "http://localhost:8080/lab2/Servlet1";
+url:string = "http://localhost:3000/Animal";
+
 
   constructor(private http:HttpClient) { }
 
   getAnimals():Observable<Animal[]>{
     return this.http.get<Animal[]>(this.url);
+  }
+  postAnimals(animal:Animal):Observable<Animal[]>{
+    return this.http.post<Animal[]>(this.url, animal);
+  }
+  putAnimals(animal:Animal):Observable<Animal[]>{
+    return this.http.put<Animal[]>(this.url+"/"+animal.title, animal);
+  }
+  deleteAnimals(animal:Animal):Observable<Animal[]>{
+    return this.http.delete<Animal[]>(this.url+"/"+animal.title);
   }
 
 }
